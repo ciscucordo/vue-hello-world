9b167a8001196432b872fcce5dd4fbec4ac3ad1f
vcollado <victorc@onmedresearch.com>
2017-12-12 15:59:35 +0100

    fix #2

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ var appComponent = new Vue({
     todoList: [
       { text: 'Item 1' },
       { text: 'Item 2' },
-      { text: 'Item 3' }
+			{ text: 'Item 3' },
+			{ text: 'Item 4' },
     ]
   },
   methods: {  
